dffdfaecd4d5f8ed32762cfe810ffdd88e0a6122
Felipe Cardoso Vargas <128812730+Felipecardosovargas@users.noreply.github.com>
2025-05-27 15:34:05 -0300

    feat(util): create JsonMapper for object mapping

---
+++ src/main/java/com/escola/util/JsonMapper.java
@@ -0,0 +1,53 @@
+package com.escola.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+
+/**
+ * Utility class for JSON serialization and deserialization using Jackson.
+ * This class is final as it only contains static utility methods.
+ *
+ * @version 1.0
+ * @author SeuNomeAqui
+ */
+public final class JsonMapper {
+    private static final ObjectMapper objectMapper = createObjectMapper();
+
+    private JsonMapper() {} // Private constructor for utility class
+
+    private static ObjectMapper createObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule()); // For Java 8 Date/Time types like LocalDate
+        // Configure other settings if needed, e.g., date formats
+        // mapper.disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    /**
+     * Serializes an object to its JSON string representation.
+     *
+     * @param object The object to serialize.
+     * @return The JSON string.
+     * @throws JsonProcessingException if an error occurs during serialization.
+     */
+    public static String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    /**
+     * Deserializes a JSON string to an object of the specified class.
+     *
+     * @param jsonString The JSON string to deserialize.
+     * @param clazz The class of the object to create.
+     * @param <T> The type of the object.
+     * @return The deserialized object.
+     * @throws IOException if an error occurs during deserialization.
+     */
+    public static <T> T fromJson(String jsonString, Class<T> clazz) throws IOException {
+        return objectMapper.readValue(jsonString, clazz);
+    }
+
+}
